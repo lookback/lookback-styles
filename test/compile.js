4c1f2e08171fead5a7436fd30d8343b05ea079ab
Johan Brook <johan@lookback.io>
2018-04-03 13:46:10 +0200

    Refactor CSS architecture to be more functional
    Inspired by Tachyons CSS library.

--- test/compile.js
+++ test/compile.js
@@ -2,10 +2,24 @@ import test from 'ava';
 import sass from 'node-sass';
 import Path from 'path';
 
-test('Can compile all', t => {
+test('Can compile base', t => {
+  t.plan(2);
+
   const result = sass.renderSync({
     // Just try to compile the whole shebang to see if it goes through.
-    file: Path.join(__dirname, '../lookback/all.scss')
+    file: Path.join(__dirname, '../lookback/base.scss')
+  });
+
+  t.truthy(result, 'could compile');
+  t.truthy(typeof result.css.toString() === 'string', 'generated CSS is a String');
+});
+
+test('Can compile all', t => {
+  t.plan(2);
+
+  const result = sass.renderSync({
+    // Just try to compile the whole shebang to see if it goes through.
+    file: Path.join(__dirname, './all.scss')
   });
 
   t.truthy(result, 'could compile');
@@ -13,6 +27,8 @@ test('Can compile all', t => {
 });
 
 test('Can compile a single partial file', t => {
+  t.plan(2);
+
   const result = sass.renderSync({
     file: Path.join(__dirname, './partial.scss')
   });
